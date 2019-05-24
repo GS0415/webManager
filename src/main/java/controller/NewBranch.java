802961b77801f5d1126e12b1321ef8262fcc72eb
guoshuai <guoshuai@atominds.com>
2019-05-24 11:47:24 +0800

    manager新分支创建测试

---
+++ src/main/java/controller/NewBranch.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * @author GuoShuai
+ * @date 2019-05-24
+ */
+public class NewBranch {
+    public static void main(String[] args) {
+        System.out.println("新建分子");
+    }
+}
